d070327c0989cd0b1a65aeee8faa9742fa5e3389
J Fixby <github@jfixby.com>
2016-01-12 15:59:53 +0100

    add  .gitignore-fix

---
+++ jfixby-git-eclipse-namefixer/jfixby-tool-git-eclipse-namefixer/com/jfixby/tool/git/eclipse/name/AddGitIgnore.java
@@ -0,0 +1,65 @@
+package com.jfixby.tool.git.eclipse.name;
+
+import java.io.IOException;
+
+import com.jfixby.cmns.api.file.ChildrenList;
+import com.jfixby.cmns.api.file.File;
+import com.jfixby.cmns.api.file.LocalFileSystem;
+import com.jfixby.cmns.api.log.L;
+import com.jfixby.cmns.desktop.DesktopAssembler;
+
+public class AddGitIgnore {
+
+	static boolean WRITE_MODE = true;
+
+	public static void main(String[] args) throws IOException {
+		DesktopAssembler.setup();
+
+		String java_path = "D:\\[DEV]\\[GIT-2]";
+		File git_folder = LocalFileSystem.newFile(java_path);
+		scanFolder(git_folder);
+
+	}
+
+	private static void scanFolder(File folder) throws IOException {
+		ChildrenList children = folder.listChildren();
+
+		File git_ignore = children.findChild(".gitignore");
+
+		if (git_ignore != null) {
+			L.d("delete", git_ignore);
+			 git_ignore.delete();
+		}
+
+		File project_file = children.findChild(".project");
+		if (project_file != null) {
+			// L.d("found", project_file);
+			check(folder, project_file);
+			return;
+		}
+
+		for (int i = 0; i < children.size(); i++) {
+			File child = children.getElementAt(i);
+			if (child.isFolder()) {
+				scanFolder(child);
+				continue;
+			}
+			if (child.isFile()) {
+				continue;
+			}
+		}
+	}
+
+	private static void check(File folder, File project_file) throws IOException {
+		File git_ignore = project_file.parent().child(".gitignore");
+		// if (!git_ignore.exists()) {
+		// return;
+		// }
+		// L.d("found git ignore", git_ignore);
+		// String data = git_ignore.readToString();
+		// L.d("data", data);
+		git_ignore.writeString("/bin/");
+		L.d("write", git_ignore);
+
+	}
+}
