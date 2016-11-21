1c2ad698d006e4dafd054764693508f93d6fde01
J Fixby <github@jfixby.com>
2016-11-21 15:18:42 +0100

    ...

--- jfixby-git-eclipse-namefixer/jfixby-tool-git-eclipse-namefixer/com/jfixby/tool/git/eclipse/name/FixGitProjects.java
+++ jfixby-git-eclipse-namefixer/jfixby-tool-git-eclipse-namefixer/com/jfixby/tool/git/eclipse/name/FixGitProjects.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.tool.git.eclipse.name;
 
 import java.io.IOException;
@@ -12,18 +13,18 @@ public class FixGitProjects {
 
 	static boolean WRITE_MODE = true;
 
-	public static void main(String[] args) throws IOException {
+	public static void main (final String[] args) throws IOException {
 		DesktopSetup.deploy();
 
-		String java_path = "D:\\[DEV]\\[GIT]";
-		File git_folder = LocalFileSystem.newFile(java_path);
+		final String java_path = "D:\\[DEV]\\[GIT]";
+		final File git_folder = LocalFileSystem.newFile(java_path);
 		scanFolder(git_folder);
 
 	}
 
-	private static void scanFolder(File folder) throws IOException {
-		ChildrenList children = folder.listDirectChildren();
-		File project_file = children.findChild(".project");
+	private static void scanFolder (final File folder) throws IOException {
+		final ChildrenList children = folder.listDirectChildren();
+		final File project_file = children.findChild(".project");
 		if (project_file != null) {
 			// L.d("found", project_file);
 			check(folder, project_file);
@@ -31,7 +32,7 @@ public class FixGitProjects {
 		}
 
 		for (int i = 0; i < children.size(); i++) {
-			File child = children.getElementAt(i);
+			final File child = children.getElementAt(i);
 			if (child.isFolder()) {
 				scanFolder(child);
 				continue;
@@ -42,14 +43,14 @@ public class FixGitProjects {
 		}
 	}
 
-	private static void check(File folder, File project_file) throws IOException {
-		String folder_name = folder.getName();
-		String data = project_file.readToString();
-		String NAME_OPEN = "<name>";
-		String NAME_CLOSE = "</name>";
-		int open = data.indexOf(NAME_OPEN) + NAME_OPEN.length();
-		int close = data.indexOf(NAME_CLOSE);
-		String project_name = data.substring(open, close);
+	private static void check (final File folder, final File project_file) throws IOException {
+		final String folder_name = folder.getName();
+		final String data = project_file.readToString();
+		final String NAME_OPEN = "<name>";
+		final String NAME_CLOSE = "</name>";
+		final int open = data.indexOf(NAME_OPEN) + NAME_OPEN.length();
+		final int close = data.indexOf(NAME_CLOSE);
+		final String project_name = data.substring(open, close);
 		if (!project_name.equals(folder_name)) {
 			L.d("renaming", folder);
 			L.d("    from", folder_name);
